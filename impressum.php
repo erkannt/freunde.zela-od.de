b7610212a57459d81719ee3e045cb8389dbebf1a
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2020-11-03 20:17:03 +0000

    original state

---
+++ impressum.php
@@ -0,0 +1,7 @@
+<?PHP
+include_once("../../impressum_dse.php");
+echo "<html><body>";
+echo impressum("*","im Auftrag des Vereins der Zeltlagerfreunde e.V.");
+echo dse("*");
+echo "</body></html>";
+ ?>
---
+++ index.php
@@ -0,0 +1,161 @@
+<html><head><title>Verein der Zeltlagerfreunde</title>
+<body BgColor="#900000" Text="#FFFFFF" Link="#FFFF00" vLink="#FFFF00" aLink="#00ff00">
+<style>
+body{font-size:4.8mm}
+td{font-size:4.8mm}
+.dflt{font-size:4.8mm;color:#ffffff}
+.small{font-size:3.5mm;color:#ffffff}
+.hi{color:#ffff00}
+a:hover {text-decoration: underline ; color:#00d0ff}
+</style>
+<table width=100% height=100%>
+<!-- ----------------------------------------------------------------------------- TITELZEILE -->
+	<tr height=1>
+		<td align=left>
+			<span style="color:#efefef; font-family: Comic Sans MS; font-size:9mm"><b>
+			Verein der Zeltlagerfreunde Bad Oldesloe e.V.
+			</b></span>
+		</td>
+		<td valign=top align=right><img src="gifs/daumen-w.gif" width=54 height=58></td>
+	</tr>
+
+	<tr height=1><td colspan=2><hr></td></tr>
+
+<!-- ----------------------------------------------------------------------------- INFOTEXT -->
+	<tr>
+		<td align=left valign=top>
+			Wir, die Zeltlagerfreunde Bad Oldesloe e.V. sind ein privater Verein, der sich zum Ziel gesetzt hat,
+			die Arbeit des Zeltlagerteams der katholischen Jugend Bad Oldesloe sowohl finanziell als auch materiell
+			zu unterst&uuml;tzen.
+			<p>
+			Das Zeltlagerteam ist seit jeher bestrebt, den Teilnehmerbeitrag so gering wie m&ouml;glich und f&uuml;r jederman
+			erschwinglich zu halten.
+			Durch die Kostenexplosion der Vergangenheit ist dieses so nicht mehr m&ouml;glich.
+			<p>
+			Diese L&uuml;cke wollen wir mit unserem Verein abdecken und <b>SIE</b> k&ouml;nnen uns dabei unterst&uuml;tzen!
+			<br>
+			Egal, ob sie aktives oder f&ouml;rderndes Mitglied unseres Vereins werden, jede Hilfe ist uns willkommen.
+			<p>
+			Bei Interesse melden Sie sich bitte bei
+			<p>
+<!-- -----------------------------------------------------------------INFOTEXT- KONTAKT -->
+			<table><!-- TABLE Kontakt -->
+				<tr>
+					<td>
+						Thorsten Mitzlaff<br>
+						L&uuml;becker Stra&szlig;e 95<br>
+						23843 Bad Oldesloe
+					</td>
+					<td width=25%></td>
+					<td>
+						Torben Reetz<br>
+						Hedwig-Kettler-Stra&szlig;e 19<br>
+						23843 Bad Oldesloe
+					</td>
+				</tr>
+
+				<tr>
+					<td colspan=3 align=center>
+						<br>
+						oder Tel.: 04531 / 89 28 120<br>
+						oder eMail: <a href="mailto:freunde@zela-od.de">freunde@zela-od.de</a>
+					</td>
+				</tr>
+			</table><!-- Ende TABLE Kontakt -->
+		</td>
+
+<!-- ----------------------------------------------------------------------------- LOGO -->
+		<td align=center valign=top>
+			<img src="logow.gif">
+			<br>
+			<a href="http://www.zela-od.de">Hier geht's zum Zeltlager</a>
+		</td>
+	</tr>
+
+<!-- ----------------------------------------------------------------------------- BANKVERBINDUNG -->
+	<tr>
+		<td align=top align=left>
+			<hr>
+			Bankverbindung:<br>
+			Volksbank Stormarn eG<br>
+			IBAN DE24 2019 0109 0041 3239 40
+		</td>
+		<td align=right valign=bottom>
+			<?PHP include("../../hitcount.php");echo hitcountptr("vdzf","../../")." Besucher"; ?>
+			<br><a href="impressum.php">Impressum und Datenschutzerkl&auml;rung</a>
+		</td>
+	</tr>
+
+	<tr height=1><td colspan=2><hr></td></tr>
+
+<!-- ----------------------------------------------------------------------------- AKTIVIT&auml;TEN-BLOG -->
+	<tr>
+		<td colspan=2>
+			<span style="color:#efefef; font-family: Comic Sans MS; font-size:5mm"><b>
+			Und hier ein &Uuml;berblick &uuml;ber unsere bisherigen Unterst&uuml;tzungen f&uuml;r das Zeltlager:
+			</b></span>
+			<p>
+			<table border=0 width=100% cellspacing=4 cellpadding=4>
+				<tr>
+					<td valign=top><img src="pics/sezelt.jpg"></td><td valign=top>ein neues SE-Zelt</td>
+					<td valign=top><img src="pics/julakueche.jpg"></td><td valign=top>eine Erweiterung f&uuml;r das K&uuml;chenzelt des Jugendlagers</td>
+				</tr>
+				<tr>
+					<td valign=top><img src="pics/moppel.jpg"></td><td valign=top>ein neues Stromaggregat</td>
+					<td valign=top><img src="pics/teamerzelte.jpg"></td><td valign=top>drei neue Gruppenleiterzelte</td>
+				</tr>
+				<tr>
+					<td valign=top><img src="pics/festzeltgarnituren.jpg"></td><td valign=top>10 neue Garnituren Tische und B&auml;nke</td>
+					<td valign=top><img src="pics/tagesausflug.jpg"></td><td valign=top>Zuschuss zum Tagesausflug des Zeltlagers</td>
+				</tr>
+				<tr>
+					<td valign=top><img src="pics/loire.jpg"></td><td valign=top>ein neues Nachtw&auml;chterzelt</td>
+					<td valign=top><img src="pics/ohnebild.jpg"></td><td valign=top>eine neue Leinwand f&uuml;r die Filmvorf&uuml;hrungen des Zeltlagerfilms</td>
+				</tr>
+				<tr>
+					<td valign=top><img src="pics/bollerwagen.jpg"></td><td valign=top>einen neuen Bollerwagen</td>
+					<td valign=top><img src="pics/ohnebild.jpg"></td><td valign=top>eine neue Kleinraum-Jurte</td>
+				</tr>
+				<tr>
+					<td valign=top><img src="pics/festplatte.jpg"></td><td valign=top>eine Festplatte zum Archivieren von Fotos und Filmen des Zeltlagers</td>
+					<td valign=top><img src="pics/hockerkocher.jpg"></td><td valign=top>zwei neue Hockerkocher f&uuml;r die K&uuml;che</td>
+				</tr>
+				<tr>
+					<td valign=top><img src="pics/videokamera.jpg"></td><td valign=top>eine neue Videokamera</td>
+					<td valign=top><img src="pics/GRJ.jpg"></td><td valign=top>eine neue Gro&szlig;raum-Jurte</td>
+				</tr>
+				<tr>
+					<td valign=top><img src="pics/schwerlastregal.jpg"></td><td valign=top>zwei Schwerlastregale f&uuml;r die neuen Lagerr&auml;ume</td>
+					<td valign=top><img src="pics/wandregal.jpg"></td><td valign=top>Wandregale f&uuml;r die neuen Lagerr&auml;ume</td>
+				</tr>
+				<tr>
+					<td valign=top><img src="pics/versammlungszelt.jpg"></td><td valign=top>eine neue Zelthaut f&uuml;r das Versammlungszelt</td>
+					<td valign=top><img src="pics/kicker.jpg"></td><td valign=top>einen neuen Tischkicker</td>
+				</tr>
+				<tr>
+					<td valign=top><img src="pics/werkzeug.jpg"></td><td valign=top>diverses Werkzeug</td>
+					<td valign=top><img src="pics/naehmaschine.jpg"></td><td valign=top>eine Industrien&auml;hmaschine f&uuml;r die Reparatur und Instandhaltung von Zelten</td>
+				</tr>
+				<tr>
+					<td valign=top><img src="pics/therme.jpg"></td><td valign=top>Wartung und Reparatur der Gastherme</td>
+					<td valign=top><img src="pics/finanzierung.jpg"></td><td valign=top>Vorfinanzierung des aktuellen Zeltlagers nach Bedarfsermittlung</td>
+				</tr>
+				<tr>
+					<td valign=top><img src="pics/teamerzelte2.jpg"></td><td valign=top>zwei neue Gruppenleiterzelte</td>
+					<td valign=top><img src="pics/kanthoelzer.jpg"></td><td valign=top>Konstruktionsh&ouml;lzer f&uuml;r die Fu&szlig;b&ouml;den von K&uuml;che und Ger&auml;tezelt</td>
+				</tr>
+				<tr>
+					<td valign=top><img src="pics/northstar.jpg"></td><td valign=top>neue Gaslampen inklusive Transportkisten</td>
+					<td valign=top><img src="pics/doppelhub.jpg"></td><td valign=top>neue Doppelhub-Luftpumpen</td>
+				</tr>
+				<tr>
+					<td valign=top><img src="pics/nacht.jpg"></td><td valign=top>batteriebetriebene Lampen f&uuml;r die Kinderzelte</td>
+<!--				<td valign=top><img src="pics/ohnebild.jpg"></td><td valign=top></td> -->
+				</tr>
+			</table>
+		</td>
+	</tr>
+</table>
+<?PHP $gimmickpath='../../gimmicks';include($gimmickpath.'/gimmicks.php');echo gimmicks($gimmickpath); ?>
+</body>
+</html>
