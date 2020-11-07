3a5fe78e5133da4c5b04e98c15a1a9bb97ac1150
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2020-11-07 11:49:35 +0000

    Replace impressum.php with styled html

--- impressum.php
+++
@@ -1,7 +0,0 @@
-<?PHP
-include_once("../../impressum_dse.php");
-echo "<html><body>";
-echo impressum("*","im Auftrag des Vereins der Zeltlagerfreunde e.V.");
-echo dse("*");
-echo "</body></html>";
- ?>
